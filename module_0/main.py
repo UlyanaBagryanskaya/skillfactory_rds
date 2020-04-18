b9dbc809b7d95667425518e0d1fc44061ca3b293
UlyanaBagryanskaya <uyubagryanskaya@gmail.com>
2020-04-18 22:11:58 +0300

    Added main.py

--- module_0/main.py
+++ module_0/main.py
@@ -1,1 +1,21 @@
-print("Hello world")
+a=1
+b=100
+import random
+k = random.randint(a,b)
+noun = b/2
+count = 2
+pop = 1
+while noun != k:
+    herochka = b/(2*count)
+    if herochka < 1:
+        herochka = 1
+    else:
+        herochka = int(herochka)
+    if k > noun:
+        noun = noun + herochka
+    if k < noun:
+        noun = noun - herochka
+    count = count * 2
+    pop = pop + 1
+
+print(k,pop)
